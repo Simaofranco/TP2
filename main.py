a2e4091864b2d56b32b7b1de0af13d8fe28939ec
Simaofranco <ekillyhg@gmail.com>
2022-12-07 16:12:18 +0000

    Criei as variaveis que vamos usar no menu do gerente

---
+++ main.py
@@ -0,0 +1,8 @@
+evento_1 = {'nome':'', 'artista':'', 'data':'', 'hora':'', 'local':''}
+evento_2 = {'nome':'', 'artista':'', 'data':'', 'hora':'', 'local':''}
+evento_3 = {'nome':'', 'artista':'', 'data':'', 'hora':'', 'local':''}
+
+lojas_fisicas = []
+alimentacao = []
+lojas_recordacoes = []
+lugares = []
