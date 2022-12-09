b12eb20ef20cd661e50ae50c6740c116508e04dc
Simaofranco <ekillyhg@gmail.com>
2022-12-09 11:43:05 +0000

    Modelo do menu clientes criado

--- main.py
+++ main.py
@@ -3,39 +3,67 @@ evento_2 = {'nome':'', 'artista':'', 'data':'', 'hora':'', 'local':''}
 evento_3 = {'nome':'', 'artista':'', 'data':'', 'hora':'', 'local':''}
 
 bancadas = ['A','B','C','D']
-
-
+bilhetes_1 = ['A1','A2','A3','A4','A5','B1','B2','B3','B4','B5','C1','C2','C3','C4','C5','D1','D2','D3','D4','D5',]
+bilhetes_2 = ['A1','A2','A3','A4','A5','B1','B2','B3','B4','B5','C1','C2','C3','C4','C5','D1','D2','D3','D4','D5',]
+bilhetes_3 = ['A1','A2','A3','A4','A5','B1','B2','B3','B4','B5','C1','C2','C3','C4','C5','D1','D2','D3','D4','D5',]
 
 
 while True:
     utilizador = input('É gerente(g) ou cliente(c):')
     if utilizador == 'g':
-        print("""
-            #############################################
-            #                Menu Gerente               #
-            #############################################
-            |                                           |
-            |              1-Criar evento               |
-            |              2- Editar evento             |
-            |              3- Eliminar evento           |
-            |                                           |
-            |                  4-sair                   |
-            |                                           |                                                    
-            |                                           |
-            #############################################
-        """)
-        opcao = int(input('\nInsira uma opção:'))
-        if(opcao == 1):
-            pass
-        elif(opcao == 2):
-            pass
-        elif(opcao == 3):
-            pass
-        elif(opcao == 4):
-            exit()
+        while True:
+            print("""
+                #############################################
+                #                Menu Gerente               #
+                #############################################
+                |                                           |
+                |              1-Criar evento               |
+                |              2-Editar evento              |
+                |              3-Eliminar evento            |
+                |              4- Ver bilhetes              |
+                |                 5-sair                    |
+                |                 6-voltar                  |                                                    
+                |                                           |
+                #############################################
+            """)
+            opcao = int(input('\nInsira uma opção:'))
+            if(opcao == 1):
+                pass
+            elif(opcao == 2):
+                pass
+            elif(opcao == 3):
+                pass
+            elif(opcao == 4):
+                exit()
+            elif(opcao == 5):
+                break
+            elif(opcao == 5):
+                break
+            else:
+                print('\nInsira uma opção valida!')
 
     elif utilizador == 'c':
-        pass
+        while True:
+            print('''
+                *********************************************
+                *                 Bem-Vindo                 *
+                *********************************************
+                |                                           |
+                |                1-Ver eventos              |
+                |               2-Comprar bilhetes          |            
+                |                                           | 
+                |                                           |
+                |                                           |
+                |___________________________________________|                                         
+
+        ''')
+            opcao = int(input('\nInsira uma opção:'))
+            if(opcao == 1):
+                pass
+            elif(opcao == 2):
+                pass
+            else:
+                print('\nInsira uma opção valida:')
     else:
         print('Insira um utilizador valido(g/c)')
         
