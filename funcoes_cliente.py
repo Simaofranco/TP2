d9d4e346eb854d6468a92e4e2faff5a0ed556368
Veyhatm <a81688@ualg.pt>
2022-12-11 15:26:51 +0000

    Criei uma função para comprar bilhetes

--- funcoes_cliente.py
+++ funcoes_cliente.py
@@ -14,7 +14,29 @@ def ver_eventos(dic1, dic2, dic3):
     for keys in dic3.keys():
         print(keys+": "+dic3[keys]+"\n")
 
+
+def comprar_bilhetes(bancadas, list):
+    """
+    Mostra o preço de cada bancada, guarda o bilhete que o cliente quer comprar, 
+    verifica se o bilhete está disponivel, adiciona a mensagem vendido à lista
+    dos bilhetes, e diz que o bilhete foi comprado
+    """
+    print('Preços:\n')
+    for keys in bancadas.keys():
+        print('Bancada '+keys+': '+bancadas[keys]+'\n')
     
+    while True:
+        bilhete = input('Qual o bilhete que quer comprar(A1-A5):')
+        if bilhete in list:
+            break
+        else:
+            print('O bilhete já esta vendido ou não existe!\n')
+    
+    for i in range(len(list))
+        if list[i] == bilhete:
+            list[i] = str(list[i]+'- vendido')
+
+    print('\nComprou o bilhete '+bilhete)
 
 
     
